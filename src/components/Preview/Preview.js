732d25b339e37499dd7499f1577eea29723ecd4a
Allison Ause <allison.ause@gmail.com>
2022-09-14 12:13:13 -0700

    wired up state for subtitle

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './Preview.css';
 
-export default function Preview({ title }) {
+export default function Preview({ title, subtitle }) {
   // implement a Preview screen here
   //  Note - the HTML should have the following structure
   //  the main div should have a class of preview and the font-name prop
@@ -18,7 +18,7 @@ export default function Preview({ title }) {
     <div className="preview">
       <h1>Blog Preview</h1>
       <h2>{title}</h2>
-      <h3>Subtitle</h3>
+      <h3>{subtitle}</h3>
       <p>Text Content</p>
     </div>
   );
